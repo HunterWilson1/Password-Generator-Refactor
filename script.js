19b8f2a5f77608675fa0af737846dfd5a8905d09
Hunter Wilson <huntertheguy123@gmail.com>
2023-01-11 17:20:22 -0600

    I added a function to collect the information for the criteria of the password and began working on the length prompt

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var passwordCriteria = getPasswordCriteria();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function getPasswordCriteria(){
+  var passwordCriteria = {};
+}
+
+// Length
+
+var length = prompt("Enter the length of the password (at least 8 and 128 characters");
+
