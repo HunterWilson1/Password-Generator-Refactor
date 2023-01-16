9501248d489cae1275e942d60a1ab3c0f19cf498
Hunter Wilson <huntertheguy123@gmail.com>
2023-01-16 12:27:29 -0600

    I added code that should ask what the user wants for the password but I don't know if it works tbh

--- script.js
+++ script.js
@@ -7,31 +7,44 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function getPasswordCriteria(){
+function getPasswordCriteria() {
   var passwordCriteria = {};
+
+  // Length
+
+  var length = prompt(
+    "Enter the length of the password (at least 8 and 128 characters)"
+  );
+
+  // Prompts for inclusions
+
+  var lowercase = confirm("Include lowercase characters?");
+  passwordCriteria.lowercase = lowercase;
+
+  var uppercase = confirm("include uppercase characters?");
+  passwordCriteria.uppercase = uppercase;
+
+  var numeric = confirm("include numberic characters?");
+  passwordCriteria.numeric = numeric;
+
+  var special = confirm("Include special characters?");
+  passwordCriteria.special = special;
+
+  // Validation
+
+  var anySelected = lowercase || uppercase || numeric || special;
+  while (!anySelected) {
+    alert("Select character types for password");
+    lowercase = confirm("Include lowercase characters?");
+    uppercase = confirm("Include uppercase characters?");
+    numeric = confirm("Include numeric characters?");
+    special = confirm("Include speical characters?");
+    anySelected = lowercase || uppercase || numeric || special;
+  }
+  return passwordCriteria;
 }
-
-// Length
-
-var length = prompt("Enter the length of the password (at least 8 and 128 characters");
-
-
-// Prompts for inclusions
-
-var lowercase = confirm("Include lowercase characters?");
-
-
-var uppercase = confirm("include uppercase characters?");
-
-var numeric = confirm("include numberic characters?")
-
-var special = confirm("Include special characters?")
-
-// Validation
-
