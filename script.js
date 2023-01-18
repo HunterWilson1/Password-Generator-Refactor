bf0e24d32a917dcbdb6b3212e26e86eaf8e1ee9a
Hunter Wilson <huntertheguy123@gmail.com>
2023-01-18 12:48:36 -0600

    I made a few changes so that the character string resets the values now instead of keeping the old ones

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 const lower = "abcdefghijklmnopqrstuvwxyz";
 
@@ -18,46 +17,53 @@ const numb = "01234546789";
 
 const spec = "#$%&()*+<=>?@";
 
-let char ="";
-
+let char;
 //first function is to generate a password. I used if/else statements to select the criteria
 function generatePassword() {
+  char = "";
   let passwordLength = prompt("Choose a length between 8 and 128 characters.");
-  if (passwordLength < 8 || passwordLength > 128) {
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) ) {
     alert("Please enter a number between 8 and 128");
+    generatePassword()
   } else {
     let lowerCase = confirm("Do you want to use lower case characters?");
-    if(lowerCase){
-      char += lower
-    };
+    if (lowerCase) {
+      char += lower;
+    }
     let upperCase = confirm("Do you want to use upper case characters?");
-    if(upperCase){
-      char += upper
-    };
+    if (upperCase) {
+      char += upper;
+    }
     let number = confirm("Do you want to use numeric characters?");
-    if (number){
-      char += numb
-    };
+    if (number) {
+      char += numb;
+    }
     let special = confirm("Do you want to use special characters?");
-    if (special){
-      char += spec
-    };
+    if (special) {
+      char += spec;
+    }
 
     if (
       lowerCase === false &&
       upperCase === false &&
       special === false &&
       number === false
-    ){
+    ) {
       alert("Please select one charcter type.");
       generatePassword();
     }
+
+    console.log(char)
   }
 
   //
   let password = "";
   for (var i = 0; i < passwordLength; i++) {
     password += char.charAt(Math.floor(Math.random() * char.length));
+    console.log(password)
   }
   return password;
 }
+
+generateBtn.addEventListener("click", writePassword);
+
